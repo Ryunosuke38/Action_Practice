d51b97c80ae0a0010f358ad86922326953c5037a
Ryunosuke Murata <ryunosuke77552@gmail.com>
2017-02-23 04:31:47 +0900

    Import Azalea

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class PlayerMove : MonoBehaviour {
 
+    public float speed = 15.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +15,23 @@ public class PlayerMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        //プレイヤーを移動させる
+        CharacterController controller = GetComponent<CharacterController>();
+        
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
 	
 	}
 }
